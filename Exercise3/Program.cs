396253fe92cae740c6282ab7baff0534fa3d119f
A.VOLODIN <cursedkun@gmail.com>
2020-12-30 21:25:31 +0300

    Добавление блока инициализации после создания объекта

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -6,9 +6,11 @@ namespace Exercise3
     {
         static void Main(string[] args)
         {
-            Employee employee = new Employee("Иван", "Иванов");
-            employee.Position = new Position(Position.director, 100000);
-            employee.Seniority = 3;
+            Employee employee = new Employee("Иван", "Иванов")
+            {
+                Position = new Position(Position.director, 100000),
+                Seniority = 3
+            };
             employee.CalculateSalary();
 
             Console.WriteLine($"Имя: {employee.FirstName}, Фамилия: {employee.LastName}, Должность: {employee.Position.Name}, Оклад: {employee.Salary:0.00}, Налоговый сбор: {(employee.Salary * 0.18):0.00}");
